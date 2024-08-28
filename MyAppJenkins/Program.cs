467889da4a91fe416cb07c2f744d2ea8537f1042
Divyansh Nama <divyansh.nama@planview.com>
2024-08-28 12:00:24 +0530

    divyansh

---
+++ MyAppJenkins/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyAppJenkins;
+
+
+
+
+Calculator calc = new Calculator();
+int result = calc.Add(5, 3); 
+Console.WriteLine($"The result of Add is: {result}");
